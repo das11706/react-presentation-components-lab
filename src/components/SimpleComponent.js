178ee9eeb85b5f804012b3f11ae17253cd862d3d
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-14 00:07:12 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,35 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mood: "happy",
+    };
+  }
+
+  // handleClick = () => {
+  //   this.setState((previousState) => {
+  //     return {
+  //       mood: !previousState.mood,
+  //     };
+  //   });
+  // };
+
+  handleClick = () => {
+    this.setState({
+        mood: "sad",
+    });
+  };
+
+  render() {
+    return(
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+      // <div onClick={this.handleClick}>{this.state.mood ? "happy" : "sad"}</div>
+    )
+  }
+}
+
+export default SimpleComponent
